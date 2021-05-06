8da98090c0c138aca4fb8ad8b0df742a688cd8ad
NathanZC <nathanccox52@gmail.com>
2021-05-06 01:56:22 -0400

    cleaner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import './App.css';
-import Header from './components/header.js'
-import Body from './components/body.js'
-
+import React from "react";
+import "./App.css";
+import Header from "./components/header.js";
+import Body from "./components/body.js";
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
       <div className="App">
         <Header />
--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,72 @@
-import React from 'react';
-import './header.css'
-import Dropdown from './dropdown.js'
-import ig from './instagram.png'
-import yt from './youtube.png'
-import spotify from './spotify.png'
-import apple from './apple.png'
-
+import React from "react";
+import "./header.css";
+import ig from "./instagram.png";
+import yt from "./youtube.png";
+import spotify from "./spotify.png";
+import apple from "./apple.png";
 
 class Header extends React.Component {
-  render(){
+  render() {
     return (
       <div className="App header middle">
-      <ul className='pull'>
-      <li className='grow2'><a href='#About'  className='headerLink' >About</a></li>
-      <li className='grow2'><a href='#Music'  className='headerLink' >Music</a></li>
-      <li className='grow2'><a href='#Dance' className='headerLink' >Dance</a></li>
+        <ul className="pull">
+          <li className="grow2">
+            <a href="#About" className="headerLink">
+              About
+            </a>
+          </li>
+          <li className="grow2">
+            <a href="#Music" className="headerLink">
+              Music
+            </a>
+          </li>
+          <li className="grow2">
+            <a href="#Dance" className="headerLink">
+              Dance
+            </a>
+          </li>
+        </ul>
+        <ul className="push">
+          <li className="grow2">
+            <a
+              href="https://www.instagram.com/kalan_zc/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={ig} alt="instagram logo" />
+            </a>
+          </li>
+          <li className="grow2">
+            <a
+              href="https://www.youtube.com/user/5Kaligula"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={yt} height={"110px"} alt="youtube logo" />
+            </a>
+          </li>
+          <li className="grow2">
+            <a
+              href="https://open.spotify.com/artist/7lqqUECLiWd1mDiG9cvQte"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={spotify} alt="spotify logo" />
+            </a>
+          </li>
+          <li className="grow2">
+            <a
+              href="https://music.apple.com/ca/artist/kalan/1482680832"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={apple} alt="apple logo" />
+            </a>
+          </li>
+        </ul>
 
-      </ul>
-      <ul className='push'>
-      <li className='grow2'><a href='https://www.instagram.com/kalan_zc/' target="_blank"><img src={ig} /></a></li>
-      <li className='grow2'><a href='https://www.youtube.com/user/5Kaligula' target="_blank"><img src={yt} height={"110px"} /></a></li>
-      <li className='grow2'><a href='https://open.spotify.com/artist/7lqqUECLiWd1mDiG9cvQte' target="_blank"><img src={spotify} /></a></li>
-      <li className='grow2'><a href='https://music.apple.com/ca/artist/kalan/1482680832' target="_blank"><img src={apple} /></a></li>
-      </ul>
-
-      <p>
-                 KALAN          
-      </p>
-    </div>
-
+        <p>KALAN</p>
+      </div>
     );
   }
 }
