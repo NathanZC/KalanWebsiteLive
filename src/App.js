fff0487f1c5ba55fee1b57d9b746d540e840d9b3
NathanZC <nathanccox52@gmail.com>
2021-05-06 11:02:40 -0400

    trying this

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Body from "./components/body.js";
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" >
         <Header />
         <Body />
       </div>
---
+++ src/components/music.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Music extends React.Component {
+  render() {
+    return (
+      <div className="MusicSection">
+        <div className="sectionHeader">
+          <h1>MUSIC</h1>
+        </div>
+        <div className="sectionbody"></div>
+      </div>
+    );
+  }
+}
+export default Music;
